3e5ba480333eab1b0d5b7cbb316c9234d4fa24c4
Rajeshwaran <54212165+Rajeshwaran2001@users.noreply.github.com>
2021-08-16 21:33:43 +0530

    Update settings.py

--- Shopping/settings.py
+++ Shopping/settings.py
@@ -10,12 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
-from pathlib import Path
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 import django_heroku
 
-
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -63,8 +60,7 @@ ROOT_URLCONF = 'Shopping.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
