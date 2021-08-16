1efc61bc42fa1eb596d873c47148be78bb772aca
Rajeshwaran <54212165+Rajeshwaran2001@users.noreply.github.com>
2021-08-16 19:15:03 +0530

    Update settings.py

--- Shopping/settings.py
+++ Shopping/settings.py
@@ -133,3 +133,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 django_heroku.settings(locals())
 
+# Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
