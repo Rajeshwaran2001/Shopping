e2e23e3b866b83ff46dfe2cd6301eeff26cf85c2
Rajeshwaran <54212165+Rajeshwaran2001@users.noreply.github.com>
2021-09-25 22:08:19 +0530

    Update settings.py

--- Shopping/settings.py
+++ Shopping/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'productapp.apps.ProductappConfig',
-    'accounts',
+    'accounts.apps.AccountsConfig',
 
 
 
